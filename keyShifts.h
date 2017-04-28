1857feb3cd172c03102c8096c1c0ebf0ce9c0627
Sebastian Kaczor <kastianoza@gmail.com>
2017-04-28 17:07:56 +0200

    Library fixes

--- keyShifts.h
+++ keyShifts.h
@@ -1,6 +1,6 @@
 /*
-	Creates array with number of engine steps required for each key
-	Written by Sebastian Kaczor
+Creates array with number of engine steps required for each key
+Written by Sebastian Kaczor
 */
 #pragma once
 
@@ -37,47 +37,47 @@
 #define KEY_H5 30
 #define KEY_C5 31
 
-class Shifths{
+class Shifths {
 public:
-	Shifths(int dSym, int dAsym, int whiteToBlack){
-		#init based on keyboard drawing
-		shfts[KEY_F3] = 0;
-		shfts[KEY_FIS3] = shfts[KEY_F3] + whiteToBlack;
-		shfts[KEY_G3] = dAsym;
-		shfts[KEY_GIS3] = shfts[KEY_G3] + whiteToBlack;
-		shfts[KEY_A3] = dAsym+dSym;
-		shfts[KEY_AIS3] = shfts[KEY_A3] + whiteToBlack;
-		shfts[KEY_H3] = dAsym*2+dSym;
-		shfts[KEY_C4] = 4*dSym;
-		shfts[KEY_CIS4] = shfts[KEY_C4] + whiteToBlack;
-		shfts[KEY_D4] = shfts[KEY_C4] + dAsym;
-		shfts[KEY_DIS4] = shfts[KEY_D4] + whiteToBlack
-		shfts[KEY_E4] = shfts[KEY_C4] + 2*dAsym;
-		
+	Shifths(int dSym, int dAsym, int whiteToBlack) {
+		//init based on keyboard drawing
+		shifts[KEY_F3] = 0;
+		shifts[KEY_FIS3] = shifts[KEY_F3] + whiteToBlack;
+		shifts[KEY_G3] = dAsym;
+		shifts[KEY_GIS3] = shifts[KEY_G3] + whiteToBlack;
+		shifts[KEY_A3] = dAsym + dSym;
+		shifts[KEY_AIS3] = shifts[KEY_A3] + whiteToBlack;
+		shifts[KEY_H3] = dAsym * 2 + dSym;
+		shifts[KEY_C4] = 4 * dSym;
+		shifts[KEY_CIS4] = shifts[KEY_C4] + whiteToBlack;
+		shifts[KEY_D4] = shifts[KEY_C4] + dAsym;
+		shifts[KEY_DIS4] = shifts[KEY_D4] + whiteToBlack;
+		shifts[KEY_E4] = shifts[KEY_C4] + 2 * dAsym;
+
 		initRestOfShifts(dSym);
 	}
-	
-	int getShift(byte key){
+
+	int getShift(char key) {
 		//TODO error checking
 		return shifts[key];
 	}
-	
+
 private:
-	int shfts [32];
-	
-	void initRestOfShifts(int dSym){
+	int shifts[32];
+
+	void initRestOfShifts(int dSym) {
 		//12 - total keys per octave
 		//7  - white keys per octave
-		for (int j=1;j<=2;j++){
-			for (int i=0;i<12;i++){
-				shfts[KEY_F3 + 12*j +i] = shfts[KEY_F3 + i] + 7*j*dSym;
-				
-				if (12*j +i == KEY_C5) //last key check
+		for (int j = 1; j <= 2; j++) {
+			for (int i = 0; i<12; i++) {
+				shifts[KEY_F3 + 12 * j + i] = shifts[KEY_F3 + i] + 7 * j*dSym;
+
+				if (12 * j + i == KEY_C5) //last key check
 					return;
 			}
 		}
 	}
-}
+};
 
 
 
--- pianoMove.h
+++ pianoMove.h
@@ -6,106 +6,107 @@
 
 #include "keyShifts.h"
 #include <Servo.h> 
-
-class ServoController{
+class ServoController {
 public:
-	ServoController(int servoPin, int servoUpAngle, int servoDownForWhiteAngle, int servoDownForBlackAngle){
+	ServoController(int servoPin, int servoUpAngle, int servoDownForWhiteAngle, int servoDownForBlackAngle) {
 		this->servoUpAngle = servoUpAngle;
 		this->servoDownForWhiteAngle = servoDownForWhiteAngle;
 		this->servoDownForBlackAngle = servoDownForBlackAngle;
 		fingerServo.attach(servoPin);
 	}
-	
-	void whiteDown(){
-		myservo.write(servoDownForWhiteAngle);
+
+	void whiteDown() {
+		fingerServo.write(servoDownForWhiteAngle);
 	}
-	
-	void blackDown(){
-		myservo.write(servoDownForBlackAngle);
+
+	void blackDown() {
+		fingerServo.write(servoDownForBlackAngle);
 	}
-	
-	void up(){
-		myservo.write(servoUpAngle);
+
+	void up() {
+		fingerServo.write(servoUpAngle);
 	}
-	
-	
-	
+
+
+
 private:
 	Servo fingerServo;
 	int servoUpAngle, servoDownForWhiteAngle, servoDownForBlackAngle;
-}
+};
 
 
-class FingerController{
+class FingerController {
 public:
-	FingerController(int stepPin, int dirPin, int engineDelay, ServoController  * fingerServo){
+	FingerController(int stepPin, int dirPin, int engineDelay, ServoController  * fingerServo, Shifts * shifts) {
 		this->dirPin = dirPin;
 		this->stepPin = stepPin;
 		this->engineDelay = engineDelay;
 		this->fingerServo = fingerServo;
+		this->shifts = shifts;
 	}
-	
-	void calibrate(){
+
+	void calibrate() {
 		//Code for calibration from button
 		currentPos = 0;
 	}
-	
-	void moveToKey(int key){
-		int nextKeyPos = shifts[key];
-		int diff = abs(nextKeyPos-currentPos);
-		
+
+	void moveToKey(char key) {
+		int nextKeyPos = shifts->getShift(key);
+		int diff = abs(nextKeyPos - currentPos);
+
 		if (diff == 0)
 			return;
-		
+
 		if (nextKeyPos > currentPos)
 			moveRight(diff);
-		else 
+		else
 			moveLeft(diff);
-		
+
 		currentPos = nextKeyPos;
 	}
-	
-	void press(int pressTime){
+
+	void press(int pressTime) {
 		//TODO
-		fingerServo.whiteDown();
+		fingerServo->whiteDown();
 		delay(pressTime);
-		fingerServo.up();
+		fingerServo->up();
 	}
-	
-	
+
+
 private:
 	int engineDelay;
 	int currentPos;
 	int stepPin;
 	int dirPin;
 	ServoController * fingerServo;
-	
+	Shifts * shifts;
+
 	//directionPin = HIGH -> go closer to engine
 	//directionPin == stepPin -> no move
-	
-	void moveLeft(int steps){
+
+	void moveLeft(int steps) {
 		digitalWrite(dirPin, HIGH);
-		for (int i=0;i<steps;i++){
-			digitalWrite(stepPin,HIGH);
+		for (int i = 0; i<steps; i++) {
+			digitalWrite(stepPin, HIGH);
 			delay(engineDelay);
-			digitalWrite(stepPin,LOW); 
+			digitalWrite(stepPin, LOW);
 			delayMicroseconds(1);
 		}
-		digitalWrite(stepPin,HIGH);
-	}
-	
-	void moveRight(int steps){
-		digitalWrite(dirPin, LOW);
-		for (int i=0;i<steps;i++){
-			digitalWrite(stepPin,LOW);
-			delay(engineDelay);
-			digitalWrite(stepPin,HIGH); 
-			delayMicroseconds(1);
-		}
-		digitalWrite(stepPin,LOW);
+		digitalWrite(stepPin, HIGH);
 	}
 
-}
+	void moveRight(int steps) {
+		digitalWrite(dirPin, LOW);
+		for (int i = 0; i<steps; i++) {
+			digitalWrite(stepPin, LOW);
+			delay(engineDelay);
+			digitalWrite(stepPin, HIGH);
+			delayMicroseconds(1);
+		}
+		digitalWrite(stepPin, LOW);
+	}
+
+};
 
 
 
